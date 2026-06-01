c5df6d1cb9789c325571037d6518fc99c9f4dc0b
Voloshko Daniil <daniil.voloshko@nure.ua>
2026-06-01 14:54:56 +0300

    Додано файл коду Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привіт з репозиторію Git!");
+            Console.ReadKey();
+        }
+    }
+}
